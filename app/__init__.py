d3db04abdae88d1e82ac04238773e0ae7d1c654d
GidMaster <k.syrovatsky@gmail.com>
2019-04-22 19:00:22 +0300

    Added DB support

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 # import site configuration
 from config import Config
+# import ORM SQLAlchemy tools/frameworks
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-#implement configuration from config.py
 app.config.from_object(Config)
+#create DB
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
