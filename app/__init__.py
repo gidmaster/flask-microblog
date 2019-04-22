274d8cd13879c33252f818b7960bf7e04af14b2e
GidMaster <k.syrovatsky@gmail.com>
2019-04-22 17:27:30 +0300

    Added config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+# import site configuration
+from config import Config
 
 app = Flask(__name__)
+#implement configuration from config.py
+app.config.from_object(Config)
 
 from app import routes
