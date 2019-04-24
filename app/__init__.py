1276e6d163c16aa5824cd67662930ae2e7d6da8d
GidMaster <k.syrovatsky@gmail.com>
2019-04-24 14:04:57 +0300

    Ch5. Users logins

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,22 @@
 from flask import Flask
-# import site configuration
+# Import site configuration
 from config import Config
-# import ORM SQLAlchemy tools/frameworks
+# Import ORM SQLAlchemy tools/frameworks
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+# Import the Login Manager
+from flask_login import LoginManager
 
 app = Flask(__name__)
+# Add config file
 app.config.from_object(Config)
-#create DB
+# Create DB
 db = SQLAlchemy(app)
+# Apply existing migrations
 migrate = Migrate(app, db)
+# Add the login manager
+login = LoginManager(app)
+# Automatic redirect for anomymous users
+login.login_view = 'login'
 
 from app import routes, models
